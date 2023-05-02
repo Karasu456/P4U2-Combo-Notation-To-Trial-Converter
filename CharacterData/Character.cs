b6d670abdba61287ddf9322bdbf556ce98052971
Karasu456 <millerbr456@gmail.com>
2023-05-02 03:46:15 -0400

    Update Character.cs

--- CharacterData/Character.cs
+++ CharacterData/Character.cs
@@ -65,6 +65,7 @@
                 {"Roll", "CmnSurinuke\ti\tc"},
                 {"2AC", "CmnMiniJump\ti\tc"},
                 {"Hop", "CmnMiniJump\ti\tc"},
+                {"Superjump", "_HighJump\tc\tnOnly"},               
                 {"Air Turn", "CmnAirTurn\ti\tc"},
                 {"j.AC", "CmnAirTurn\ti\tc"},
                 {"BD", "ReversalAction"},
